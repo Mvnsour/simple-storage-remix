49815978bacfef85f7b55676f6facc1fbc96b223
Mvnsour <mbgmansour13@gmail.com>
2025-06-28 20:36:54 +0200

    feat: import SimpleStorage contract instead of write it directly in the file

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,33 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract SimpleStorage {
-    uint256  myFavNum;
-
-    struct Person {
-        uint256 favNum;
-        string name;
-    }
-
-    Person[] public listofPeople; 
-
-
-    mapping(string => uint256) public nameToFavNum;
-
-    function store(uint256 _favNum) public {
-        myFavNum = _favNum;
-    }
-
-    function retrieve() public view returns(uint256) {
-        return myFavNum;
-    }
-
-    function addPerson(string memory _name, uint256 _favNum) public {
-        listofPeople.push( Person(_favNum, _name) );
-        nameToFavNum[_name] = _favNum;
-    }
-
-}
+import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
