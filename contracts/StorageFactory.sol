228d8b111eba92e3271af5ac297a850de8a4a204
Mvnsour <mbgmansour13@gmail.com>
2025-06-30 02:28:25 +0200

    feat: Implement SimpleStorage contract factory  This commit introduces the core functionality for creating and interacting with SimpleStorage contracts using the StorageFactory contract.  Key changes:  - Added `simpleStorageContractList`: A dynamic array to store deployed SimpleStorage contract addresses. - Implemented `createSimpleStorageContract()`: Deploys a new SimpleStorage contract and adds its address to the list. - Implemented `sfStore()`: Allows setting the stored value of a specific SimpleStorage contract by index. - Implemented `sfGet()`: Allows retrieving the stored value of a specific SimpleStorage contract by index.  This provides the basic mechanism for deploying and managing multiple SimpleStorage contracts through a factory pattern.

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -5,9 +5,25 @@ import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageContractList;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+      // Create a new instance of the SimpleStorage contract
+      SimpleStorage newSimpleStorageContract = new SimpleStorage();
+      // Add new contract to the list
+      simpleStorageContractList.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIdx, uint256 _newSimpleStorageNum) public {     //sfStore means StorageFactoryStore //SimpleStorage store function will be called here
+      //SimpleStorage store function will be called here
+      // Address
+      // ABI (Application Binary Interface) will tell our code exactly how he can interact with another contract
+      SimpleStorage mySimpleStorage = simpleStorageContractList[_simpleStorageIdx];
+      mySimpleStorage.store(_newSimpleStorageNum);
+    }
+
+    function sfGet(uint256 _simpleStorageIdx) public view returns (uint256) {
+      SimpleStorage mySimpleStorage = simpleStorageContractList[_simpleStorageIdx];
+      return mySimpleStorage.retrieve();
     }
 }
